cc734af1e5bff091604618f83cb9d4d6568a13b3
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-07-17 23:57:00 -0300

    board project

---
+++ src/components/Subdivisao/index.jsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.css'
+
+export default function Subdivisao(props) {
+  return (
+    <div className={styles.subdivisao}></div>
+  )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Subdivisao from '../components/Subdivisao'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Subdivisao />
+
+    </div>
+  )
+}
